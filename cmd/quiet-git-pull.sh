945be878526047795be00ad5e7d2fe8f4be9d926
Oscar Nierstrasz <oscar@machupicchu.home>
2016-07-01 21:59:47 +0200

    quiet pull

--- cmd/quiet-git-pull.sh
+++ cmd/quiet-git-pull.sh
@@ -4,6 +4,6 @@ C=/home/choose/choose.s-i.ch
 G=/opt/pkg/bin/git
 H=asterix
 case `hostname` in
-	$H ) cd $C ; $G pull > /dev/null ;;
+	$H ) cd $C ; $G pull &> /dev/null ;;
 	* ) echo "please run on $H" ; exit 1 ;;
 esac
